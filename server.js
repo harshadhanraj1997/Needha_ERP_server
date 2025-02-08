efb72206e363350b0f6c1e621c5154a9bf1c1a1a
Sai Charan <charanvadhyar@gmail.com>
2025-02-08 11:04:21 +0530

    removed batch no and tree details for seperate functionality

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 app.use(cors({
   origin: [
     "http://localhost:3000", // Localhost for development
+    "http://localhost:3001",
     "https://atmalogicerp.vercel.app" // Replace with your actual Vercel frontend URL
   ],
   credentials: true, // Allow credentials (cookies, authorization headers)
