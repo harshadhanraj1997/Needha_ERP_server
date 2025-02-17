0025bca58e15f66f3707463e566c0198e2532b6e
Sai Charan <charanvadhyar@gmail.com>
2025-02-17 13:57:14 +0530

    fixed errors in api call for get casting detail

--- server.js
+++ server.js
@@ -1258,7 +1258,7 @@ app.post("/api/casting", async (req, res) => {
       Required_Pure_Metal_Casting__c: requiredMetals.pureGold,
       Required_Alloy_for_Casting__c: requiredMetals.alloy,
       Issud_weight__c: totalIssued,
-      status__c : "Pending"
+      status__c: "Pending"
     });
 
     if (!castingResult.success) {
