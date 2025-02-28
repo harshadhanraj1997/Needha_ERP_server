45f7d3d27b018bd8cdea5a710433aed5f9b10ac1
Sai Charan <charanvadhyar@gmail.com>
2025-02-28 15:28:29 +0530

    added setting api calls

--- server.js
+++ server.js
@@ -3196,7 +3196,7 @@ app.post("/api/setting/create", async (req, res) => {
       const pouchResult = await conn.sobject('Pouch__c').update({
         Id: pouch.pouchId,
         Setting__c: settingResult.id,
-        Isssued_weight_Setting__c: pouch.settingWeight,
+        Isssued_weight_setting__c: pouch.settingWeight,
         Status__c: 'In Setting'
       });
 
