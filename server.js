c4bf705fb11f2589fad18963acfe52a910866e45
Sai Charan <charanvadhyar@gmail.com>
2025-03-01 12:03:23 +0530

    added dull server calls

--- server.js
+++ server.js
@@ -3864,7 +3864,7 @@ app.get("/api/dull/:prefix/:date/:month/:year/:number/pouches", async (req, res)
         Name,
         Issued_Date__c,
         Issued_Weight__c,
-        Received_Weight__c,
+        Returned_weight__c,
         Received_Date__c,
         Status__c,
         Dull_Loss__c
