745f0f8e139ce864bc0002b1866305fea9518fcb
Sai Charan <charanvadhyar@gmail.com>
2025-03-17 15:26:23 +0530

    modified a little deatils in the setting details

--- server.js
+++ server.js
@@ -3254,9 +3254,8 @@ app.get("/api/setting", async (req, res) => {
         Returned_weight__c,
         Received_Date__c,
         Status__c,
-        Stone_Weight__c
+        Stone_Weight__c,
         Setting_l__c,
-
         CreatedDate
        FROM Setting__c
        ORDER BY CreatedDate DESC`
