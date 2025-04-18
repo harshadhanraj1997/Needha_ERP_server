99098153b2b20429461d5e9b83e76d9e701dca52
Sai Charan <charanvadhyar@gmail.com>
2025-04-18 12:39:12 +0530

    changed purities of scrap and dust

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ const axios = require('axios'); // Import axios
 var bodyParser = require('body-parser');
 
 // Configure body-parser with increased limits
-app.use(bodyParser.json({ limit: '100mb' }));  // Increase as needed
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '500mb' }));  // Increase as needed
+app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
 
 // Also increase Express limit
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ 
-  limit: '50mb',
+  limit: '500mb',
   extended: true,
   parameterLimit: 50000 
 }));
